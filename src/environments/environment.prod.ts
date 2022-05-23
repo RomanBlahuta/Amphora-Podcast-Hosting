c3cac13b3c31461fbd1a5583bc6c608c9b03adb5
RomanBlahuta <blahutaroma@gmail.com>
2022-05-23 18:39:40 +0300

    Hosting update.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+    production: true,
+    apiBase: 'https://podcast-hosting-api.herokuapp.com',
+    secret: 'd1sf4gfs-hul80r4igy7uuft-y08ft4kt-df7ukt8yty75hjgf-hj7fyuf89uy0h-g4yu98tfyul1f',
 };
